d37c34d01ad593f0dbb0cce2ba7b9f394f07311d
Sibanando Nayak <sibhanayak@hotmail.com>
2023-10-24 11:13:58 +0530

    terraform-v1

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "anil01"
+  default     =  "key"
 }
 
 variable "instance_type" {
